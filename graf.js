ec9646102e7cdac8677dbc61b3e981cf155116a1
nelakrawiecova <nela.krawiecova@gmail.com>
2021-01-19 13:06:19 +0100

    graf

--- graf.js
+++ graf.js
@@ -1,90 +1,94 @@
-// Nastaveni max sirky grafu
-const chartWidthNehody = document.getElementById('vis-column-chart-simple').offsetWidth > 600
-  ? 600
-  : document.getElementById('vis-column-chart-simple').offsetWidth;
-
 function onChartLoad(e) {
-  const plotBack = document.getElementById(e.renderTo.id).getElementsByClassName('highcharts-plot-background')[0];
-  const shouldBeHeight = (plotBack.width.baseVal.value / 4) * 3;
-  const heightDiff = shouldBeHeight - plotBack.height.baseVal.value;
-  if (heightDiff > 0) {
-    document.getElementById(e.renderTo.id).style.height = `${e.chartHeight + heightDiff}px`;
-    e.reflow();
+    const plotBack = document.getElementById(e.renderTo.id).getElementsByClassName('highcharts-plot-background')[0];
+    const shouldBeHeight = (plotBack.width.baseVal.value / 4) * 3.5;
+    const heightDiff = shouldBeHeight - plotBack.height.baseVal.value;
+    if (heightDiff > 0) {
+      document.getElementById(e.renderTo.id).style.height = `${e.chartHeight + heightDiff}px`;
+      e.reflow();
+    }
   }
-}
-
-Highcharts.chart('vis-column-chart-simple', {
-  chart: {
-    type: 'column',
-    width: chartWidthNehody,
-    events: {
-      load() {
-        onChartLoad(this);
+  
+  Highcharts.chart('graf3', {
+    chart: {
+      type: 'bar',
+      //width: chartWidthNehody,
+      events: {
+        load() {
+          onChartLoad(this);
+        },
       },
+  
     },
-  },
-  title: {
-    text: 'Column Chart Single',
-    useHTML: true,
-  },
-  subtitle: {
-    text: 'Celkový počet nepovolených průjezdů byl v&nbsp;loňském prvním pololetí vyšší než letos',
-    // + '<br><span style="color: #fff">.</span>',
-    useHTML: true,
-  },
-  xAxis: {
-    categories: ['<span class="eighteen">2018</span>',
-      '<span class="nineteen">2019</span>',
-      '<span class="twenty">2020</span>'],
-    labels: {
-      enabled: true,
-    },
-  },
-  yAxis: {
     title: {
-      text: 'nepovolené jízdy za návěstidla',
+      text: 'Nejčastější příznaky půl roku po covidu',
+      useHTML: true,
     },
-    // labels: {
-    //   formatter: function() {
-    //     if (this.isLast) {
-    //       return this.value + '<br>' +
-    //                   '<span class="light-gray-text">jízd za</span>' + '<br>' +
-    //                   '<span class="light-gray-text">návěstidla</span>'
-    //     } else {
-    //       return this.value
-    //     }
-    //   }
-    // }
-  },
-  legend: {
-    enabled: false,
-  },
-  tooltip: {
-    valueSuffix: ' jízd',
-    // shared: true
-  },
-  exporting: {
-    enabled: false,
-  },
-  credits: {
-    href: 'http://www.dicr.cz/statistiky-mimoradnych-udalosti',
-    text: 'Zdroj: Drážní inspekce',
-  },
-  plotOptions: {
-    column: {
-      dataLabels: {
+    subtitle: {
+      text: 'Mezi 1733 sledovanými pacienty po hospitalizaci',
+      // + '<br><span style="color: #fff">.</span>',
+      useHTML: true,
+    },
+    xAxis: {
+      categories: ['alespoň jeden z příznaků',
+      'únava, bolest svalů',
+      'problémy se spaním',
+      'ztráta vlasů',
+      'ztráta čichu',
+      'palpitalions',
+      'bolest kloubů',
+      labels: {
         enabled: true,
       },
-      // enableMouseTracking: false, // odkomentuj, pokud nechces tooltip
-      pointPadding: 0,
     },
-  },
-  series: [
-    {
-      name: 'Celkový počet mimořádných událostí',
-      data: [{ y: 85, color: colors['2018'] },
-        { y: 97, color: colors['2019'] },
-        { y: 88, color: colors['2020'] }],
+    yAxis: {
+      title: {
+        text: '',
+      },
+      // labels: {
+      //   formatter: function() {
+      //     if (this.isLast) {
+      //       return this.value + '<br>' +
+      //                   '<span class="light-gray-text">jízd za</span>' + '<br>' +
+      //                   '<span class="light-gray-text">návěstidla</span>'
+      //     } else {
+      //       return this.value
+      //     }
+      //   }
+      // }
     },
-  ],
-});
+    legend: {
+      enabled: false,
+    },
+    tooltip: {
+      valueSuffix: '',
+      shared: false
+    },
+    exporting: {
+      enabled: false,
+    },
+    credits: {
+      href: 'https://www.thelancet.com/journals/lancet/article/PIIS0140-67362032656-8/fulltext',
+      text: 'The Lancet: 6-month consequences of COVID-19 in patients discharged from hospital: a cohort study',
+    },
+    plotOptions: {
+      column: {
+        dataLabels: {
+          enabled: true,
+        },
+        // enableMouseTracking: false, // odkomentuj, pokud nechces tooltip
+        pointPadding: 0,
+      },
+    },
+    series: [
+      {
+        name: 'Index závažnosti',
+        data: [{ y: 76, color: colors['2020'] },
+          { y: 63, color: colors['2020'] },
+          { y: 26, color: colors['2020'] },
+          { y: 22, color: colors['2020'] },
+          { y: 11, color: colors['2020'] },
+          { y: 9, color: colors['2019'] },
+          { y: 9, color: colors['2020'] },
+      },
+    ],
+  });
